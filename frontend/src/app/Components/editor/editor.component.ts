4572fb77c21adfbbfa2b22a31e0d61fbb51500ab
Sahil Puniyani <punyanisahil0001@gmail.com>
2022-09-03 18:34:19 +0400

    add some changes

--- frontend/src/app/Components/editor/editor.component.ts
+++ frontend/src/app/Components/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-
+ code : any  ;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ 
+  data(code : NgForm){
+  console.log(code.value.editor);
+  this.code = code.value.editor;
+  let res = JSON.stringify(code.value.data);
+  console.log(res);
+  }
+  
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { EditorComponent } from './Components/editor/editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EditorComponent } from './Components/editor/editor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
