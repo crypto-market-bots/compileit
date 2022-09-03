837c845390d258d61898bfdf4571a93ebd655e3f
Lakshay122 <bansallakshay081@gmail.com>
2022-09-03 19:51:30 +0300

    python code deo

--- backend/compiler/views.py
+++ backend/compiler/views.py
@@ -1,3 +1,4 @@
+import sys
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,16 +7,13 @@ import json
 from base64 import b64encode, b64decode
 
 
-
-
-
 # Create your views here.
 
 class CodeUpload(APIView):
     def validate(self):
         data = json.loads(self.request.body.decode('utf-8'))
         self.language = data.get("language", '')
-        self.code_content = data.get("code",'')
+        self.code_content = data.get("code", '')
         print(self.language, self.code_content)
 
     def return_genric_response(self):
@@ -23,9 +21,20 @@ class CodeUpload(APIView):
 
     def post(self, request, format=None):
         self.validate()
-        self.result = {'msg': 'registration success'}
+        self.solve()
+
+        self.result = {'output': self.output_std}
         return self.return_genric_response()
 
+    def solve(self):
+        try:
+            original_std = sys.stdout
+            sys.stdout = open('open.txt', 'w')
+            exec(self.code_content)
+            sys.stdout.close()
+            sys.stdout = original_std
 
-#"language":"sfsdf",
-#"code":"dfdf"}
+            self.output_std = open('open.txt', 'r').read()
+        except Exception as e:
+            sys.stdout = original_std
+            self.output_std = e
